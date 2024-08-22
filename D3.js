e4bd95eec90f0cfcc0249f7ed04764545df1460e
Valerio Persiani <persianivalerio0@gmail.com>
2024-08-22 11:13:47 +0200

    EXTRA 10

--- D3.js
+++ D3.js
@@ -226,7 +226,11 @@ for (let i = 0; i < charactersNames.length; i++) {
     }
   }
 }
-console.log(charactersNames);
+console.log("PRIMA DELLA RIMOZIONE", starWarsCharacters);
+console.log("DOPO LA RIMOZIONE", charactersNames);
 /* --EXTRA-- ESERCIZIO 10
   Crea una funzionalità che selezioni un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
 */
+console.log("personaggio random");
+const random = Math.floor(Math.random() * starWarsCharacters.length);
+console.log(random);
